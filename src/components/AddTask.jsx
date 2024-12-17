a32906088650e325364166930e8cab3284a62fe3
WandesonAndrade <wandesonandrade33@gmail.com>
2024-12-17 12:21:53 -0300

    feat: add alert

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
+import { MdAddToPhotos } from "react-icons/md";
+import { useAlert } from "react-alert";
 
 import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
 
 import "./AddTask.scss";
 const AddTask = () => {
   const [task, setTask] = useState("");
 
+  const alert = useAlert();
+
   const onChange = (e) => setTask(e.target.value);
+
+  const handleTask = () => {
+    try {
+      if (task.length === 0) {
+        return alert.error("Preencha o campo de tarefa.");
+      }
+    } catch (error) {}
+  };
 
   return (
     <div className="add-task-container">
@@ -15,6 +28,10 @@ const AddTask = () => {
         value={task}
         onChange={onChange}
       />
+
+      <CustomButton onClick={handleTask}>
+        <MdAddToPhotos size={14} color="white" />
+      </CustomButton>
     </div>
   );
 };
